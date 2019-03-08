7d06598e3a989322fb2eb22244cb3ab78417dcd3
Max Melnyk <imaxmelnyk@gmail.com>
2019-03-08 23:10:45 +0200

    flat option

--- lib/util/option.ex
+++ lib/util/option.ex
@@ -3,17 +3,19 @@ defmodule Util.Option do
 
   @spec map(t(any, any), (any -> any)) :: t(any, any)
   def map({:ok, val}, succ), do: {:ok, succ.(val)}
-  def map(err, _), do: err
+  def map(opt, _), do: opt
 
   @spec map(t(any, any), (any -> any), (any -> any)) :: t(any, any)
   def map({:ok, val}, succ, _), do: {:ok, succ.(val)}
   def map({:error, err}, _, fail), do: {:error, fail.(err)}
 
+  @spec flat(t(any, any)) :: any
+  def flat({:ok, val}), do: val
+  def flat({:error, err}), do: err   # tricky one. avoid or just be careful?
+
   @spec flat_map(t(any, any), (any -> t(any, any))) :: t(any, any)
-  def flat_map({:ok, val}, succ), do: succ.(val)
-  def flat_map(err, _), do: err
+  def flat_map(opt, succ), do: map(opt, succ) |> flat()
 
   @spec flat_map(t(any, any), (any -> t(any, any)), (any -> t(any, any))) :: t(any, any)
-  def flat_map({:ok, val}, succ, _), do: succ.(val)
-  def flat_map({:error, err}, _, fail), do: fail.(err)
+  def flat_map(opt, succ, fail), do: map(opt, succ, fail) |> flat()
 end
