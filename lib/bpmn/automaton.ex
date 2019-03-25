ebb95339965eda1e54320734f4454b2b87fcddbd
Max Melnyk <imaxmelnyk@gmail.com>
2019-03-25 17:57:31 +0200

    remove taos (kinda)

--- lib/bpmn/automaton.ex
+++ lib/bpmn/automaton.ex
@@ -8,17 +8,15 @@ defmodule Bpmn.Automaton do
   alias Bpmn.Process
   alias Bpmn.Element
   alias Bpmn.Element.SequenceFlow
-  alias Bpmn.Element.Event.Start
-  alias Bpmn.Element.Event.End
-  alias Bpmn.Element.Gateway.Parallel
-  alias Bpmn.Element.Gateway.Exclusive
+  alias Bpmn.Element.Event
+  alias Bpmn.Element.Gateway
 
   @spec build_from_process(Process.t) :: __MODULE__.t
   def build_from_process(process) do
     flows =
       Enum.filter(process.elements, &SequenceFlow.is_sequence_flow/1)
     start_events =
-      Enum.filter(process.elements, &Start.is_start_event/1)
+      Enum.filter(process.elements, &Event.Start.is_start_event/1)
     start_flows =
       Enum.filter(flows, fn flow -> flow.source in Enum.map(start_events, &(&1.id)) end)
     start_states =
@@ -48,7 +46,7 @@ defmodule Bpmn.Automaton do
         |> Enum.group_by((fn {_, _, target} -> target end), (fn {flow, _, _} -> flow end))
         |> Enum.reduce(automaton, fn {target, flows}, automaton ->
           cond do
-            Parallel.is_parallel_gateway(target) ->
+            Gateway.Parallel.is_parallel_gateway(target) ->
               num_flows_with_target =
                 process.elements
                 |> Enum.filter(fn elem -> SequenceFlow.is_sequence_flow(elem) && elem.target == target.id end)
@@ -89,7 +87,77 @@ defmodule Bpmn.Automaton do
 
   @spec remove_taos(Process.t, __MODULE__.t) :: __MODULE__.t
   defp remove_taos(process, automaton) do
-    automaton
+    {automaton, removed_states} =
+      remove_taos(process, automaton, [])
+
+    new_states =
+      automaton.states -- removed_states
+    new_transitions =
+      Enum.filter(automaton.transitions, fn {from, to, _} ->
+        from not in removed_states && to not in removed_states
+      end)
+
+    %{automaton | states: new_states, transitions: new_transitions}
+  end
+
+  @spec remove_taos(Process.t, __MODULE__.t, list(%{})) :: {__MODULE__.t, list(%{})}
+  defp remove_taos(process, automaton, removed_states) do
+    automaton.transitions
+    |> Enum.reduce_while({automaton, removed_states}, fn {from, to, is_allowed}, {automaton, removed_states} ->
+      from_flows =
+        state_to_growed_flows(process, from)
+      to_flows =
+        state_to_growed_flows(process, to)
+
+      from_targets =
+        from_flows
+        |> Enum.map(fn {_, _, target} -> target end)
+        |> Enum.uniq()
+      to_sources =
+        to_flows
+        |> Enum.map(fn {_, source, _} -> source end)
+        |> Enum.uniq()
+
+      case {from_targets, to_sources} do
+        {[elem], [elem]} ->
+          cond do
+            Gateway.is_gateway(elem) ->
+              IO.inspect(elem)
+              new_state = merge_states(from, to)
+              automaton =
+                automaton
+                |> add_state(new_state)
+                |> remove_transition({from, to, is_allowed})
+
+              transitions =
+                cond do
+                  Gateway.Parallel.is_parallel_gateway(elem) ->
+                    Enum.map(automaton.transitions, fn {f, t, is_a} ->
+                      cond do
+                        f == to -> {new_state, t, is_a}
+                        t == from -> {f, new_state, is_a}
+                        true -> {f, t, is_a}
+                      end
+                    end)
+                  Gateway.Exclusive.is_exclusive_gateway(elem) ->
+                    Enum.reduce(automaton.transitions, automaton.transitions, fn {f, t, is_a}, transitions ->
+                      cond do
+                        f == to -> [{new_state, t, (fn vars -> is_a.(vars) && is_allowed.(vars) end)} | transitions]
+                        t == from -> [{f, new_state, (fn vars -> is_a.(vars) && is_allowed.(vars) end)} | transitions]
+                        true -> transitions
+                      end
+                    end)
+                end
+
+              new_automaton = %{automaton | transitions: transitions}
+              new_removed_states = removed_states ++ [from, to]
+
+              {:halt, remove_taos(process, new_automaton, new_removed_states)}
+            true -> {:cont, {automaton, removed_states}}
+          end
+        _ -> {:cont, {automaton, removed_states}}
+      end
+    end)
   end
 
   @spec state_to_growed_flows(__MODULE__.t, %{}) :: {SequenceFlow.t, Element.source_element, Element.target_element}
@@ -122,7 +190,7 @@ defmodule Bpmn.Automaton do
   @spec remove_transition(__MODULE__.t, {%{}, %{}, (%{} -> boolean)}) :: __MODULE__.t
   defp remove_transition(automaton, {dfrom, dto, _}) do
     new_transitions =
-      Enum.filter(automaton.transitions, fn {from, to, _} -> dfrom == from && dto == to end)
+      Enum.filter(automaton.transitions, fn {from, to, _} -> !(dfrom == from && dto == to) end)
 
     %{automaton | transitions: new_transitions}
   end
@@ -135,11 +203,17 @@ defmodule Bpmn.Automaton do
         true ->
           with {:ok, flow} <- Element.find_by_id(process.elements, flow_id),
                {:ok, target} <- Element.find_by_id(process.elements, flow.target) do
-            if End.is_end_event(target), do: {:cont, true}, else: {:halt, false}
+            if Event.End.is_end_event(target), do: {:cont, true}, else: {:halt, false}
           else
             _ -> raise "Something went wrong..."
           end
       end
+    end)
+  end
+
+  defp merge_states(state1, state2) do
+    Enum.map(state1, fn {flow_id, has_token} ->
+      {flow_id, has_token || Map.get(state2, flow_id, false)}
     end)
   end
 end
